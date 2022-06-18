0951bc03f041523c88a1c1240969489158329925
Mai .H <78362977+Mai-00048@users.noreply.github.com>
2022-06-18 12:35:48 +0400

    Add files via upload

---
+++ assignment_Mai/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  var lines = 15;
+  var textLength = 40;
+
+  for (i = 0; i < lines - 1; i++) {
+    $(".num").first().clone().insertAfter($(".num").first());
+  }
+
+
+  setInterval(function() {
+    $(".num").each(function() {
+      if ($(this).text().length < textLength) {
+        $(this).text(($(this).text() + Math.round(Math.random())));
+      } else {
+        $(this).text(Math.round(Math.random()) + $(this).text().substring(0, $(this).text().length - 1))
+      }
+    });
+
+  }, 95);
+
+});
